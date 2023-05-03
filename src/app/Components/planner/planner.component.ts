42ee21c24efd605c287c662855f5593bdc68dda0
Darshan Patel <darshanpatel2608ce@gmail.com>
2023-05-03 08:20:26 +0530

    added drag logic

--- src/app/Components/planner/planner.component.ts
+++ src/app/Components/planner/planner.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+interface DrawParams {
+  x: number | null;
+  y: number | null;
+  dx: number | null;
+}
+
+interface ReservationPositions{
+  top: number;
+  left: number;
+  width: number;
+}
+
 @Component({
   selector: 'app-planner',
   templateUrl: './planner.component.html',
   styleUrls: ['./planner.component.scss'],
 })
 export class PlannerComponent implements OnInit {
+  drawParams: DrawParams = {
+    x: null,
+    y: null,
+    dx: null,
+  };
+  reservations:ReservationPositions[] = [];
+
   constructor() {}
 
   ngOnInit(): void {
@@ -15,11 +34,6 @@ export class PlannerComponent implements OnInit {
     const context = canvas.getContext('2d');
     if (!context) return;
     this.drawGrid(context);
-  }
-
-  canvas_mouse_down(event:MouseEvent):void{
-    console.log(event);
-    
   }
 
   drawGrid(ctx: CanvasRenderingContext2D): void {
@@ -42,5 +56,32 @@ export class PlannerComponent implements OnInit {
       ctx.closePath();
       y += 60;
     }
+  }
+
+  canvas_mouse_down(canvasEvent: MouseEvent): void {
+    this.drawParams['x'] = canvasEvent.offsetX - (canvasEvent.offsetX%30) ;
+    this.drawParams['y'] = canvasEvent.offsetY + 4 - (canvasEvent.offsetY%60)   ;
+  }
+
+  canvas_mouse_move(canvasEvent: MouseEvent): void {
+    if (this.drawParams.x === null || this.drawParams.y === null) return;
+    const width = (canvasEvent.offsetX - this.drawParams['x']);
+    this.drawParams['dx'] = width;
+  }
+
+  canvas_mouse_up(canvasEvent: MouseEvent): void {
+    if(this.drawParams.x === null || this.drawParams.y === null || this.drawParams.dx === null) return;
+    const width = this.drawParams['dx'];
+    const newReservation:ReservationPositions = {
+      top : this.drawParams['y']-4,
+      left: this.drawParams['x'],
+      width: width%30 < 15 ? width - width%30 : width + 30 - width%30
+    }
+    this.reservations.push(newReservation);
+    this.drawParams['x'] = null;
+    this.drawParams['y'] = null;
+    this.drawParams['dx'] = null;
+    console.log(this.reservations);
+    
   }
 }
